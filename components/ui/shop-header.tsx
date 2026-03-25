e634f59e95e4ae33d553486c3b19928af283562f
Shinyn <mattias.alm@hotmail.com>
2026-03-25 15:51:03 +0100

    feat: Replace repetetive tailwind with utility classes & replace hardcoded colors

--- components/ui/shop-header.tsx
+++ components/ui/shop-header.tsx
@@ -4,49 +4,48 @@ import Link from 'next/link';
 export default function ShopHeader() {
   return (
     <>
-      <header className="bg-[#16191d] bg-[radial-gradient(#f5f2eb0f_1px,transparent_1px)] bg-size-[8px_8px] text-gray-500 flex justify-center border-b-2 border-white">
+      <header className="sticky top-0 z-50 w-full bg-(--color-effect-blue) benday-dots text-(--color-text-muted) flex justify-center border-b-2 border-white">
         <div className="flex justify-between p-4 w-full max-w-260 items-center">
           <section className="flex items-center gap-1">
-            <Zap className="size-6 text-white bg-red-500 p-1 rounded-sm border border-white " />
+            <Zap className="size-6 text-white bg-(--color-effect-red) p-1 rounded-sm border border-white " />
             <h1 className="text-white font-bold italic">Hero Vault</h1>
           </section>
 
           {/* NAVIGATION */}
           <nav>
-            {/* Selected link color sould be #E62E2E */}
-            <ul className="flex gap-2 uppercase text-xs p-2 font-bold">
+            <ul className="gap-2 uppercase text-xs p-2 font-bold hidden lg:flex">
               <li>
-                <Link className="text-[#E62E2E] hover:text-white transition-all duration-150 ease-in" href={'/'}>
+                <Link className="text-(--color-effect-red) link-hover" href={'/'}>
                   All Gear
                 </Link>
               </li>
               <li>
-                <Link className="hover:text-white transition-all duration-150 ease-in" href={'/'}>
+                <Link className="link-hover" href={'/'}>
                   Suits
                 </Link>
               </li>
               <li>
-                <Link className="hover:text-white transition-all duration-150 ease-in" href={'/'}>
+                <Link className="link-hover" href={'/'}>
                   Weapons
                 </Link>
               </li>
               <li>
-                <Link className="hover:text-white transition-all duration-150 ease-in" href={'/'}>
+                <Link className="link-hover" href={'/'}>
                   Masks
                 </Link>
               </li>
               <li>
-                <Link className="hover:text-white transition-all duration-150 ease-in" href={'/'}>
+                <Link className="link-hover" href={'/'}>
                   Capes
                 </Link>
               </li>
               <li>
-                <Link className="hover:text-white transition-all duration-150 ease-in" href={'/'}>
+                <Link className="link-hover" href={'/'}>
                   Boots
                 </Link>
               </li>
               <li>
-                <Link className="hover:text-white transition-all duration-150 ease-in" href={'/'}>
+                <Link className="link-hover" href={'/'}>
                   Tech
                 </Link>
               </li>
@@ -60,16 +59,16 @@ export default function ShopHeader() {
               <input
                 type="search"
                 placeholder="Search heroic gear..."
-                className="text-sm focus:border-[#E62E2E] outline-none focus:shadow-[0_0_8px_#E62E2E] border border-transparent pl-8 p-2 bg-[#29292c] rounded-sm"
+                className="text-sm focus:border-(--color-effect-red) outline-none focus:shadow-[0_0_8px_#E62E2E] border border-transparent pl-8 p-2 bg-[#29292c] rounded-sm"
               />
             </section>
 
             {/* ICONS */}
             <section className="flex gap-2 pl-2">
-              <Heart className="size-4 hover:text-[#E62E2E] hover:cursor-pointer transition-all duration-150 ease-in" />
-              <ShoppingBag className="size-4 hover:text-[#E62E2E] hover:cursor-pointer transition-all duration-150 ease-in" />
-              <User className="size-4 hover:text-[#E62E2E] hover:cursor-pointer transition-all duration-150 ease-in" />
-              <Menu className="size-4 hover:text-[#E62E2E] hover:cursor-pointer transition-all duration-150 ease-in" />
+              <Heart className="size-4 icon-hover" />
+              <ShoppingBag className="size-4 icon-hover" />
+              <User className="size-4 icon-hover" />
+              <Menu className="size-4 icon-hover lg:hidden" />
             </section>
           </div>
         </div>
