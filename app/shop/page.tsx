b387824734c225550baf77efaea8a5b4679edaa7
Shinyn <mattias.alm@hotmail.com>
2026-03-26 14:17:23 +0100

    feat: Add ShopTestimonials import

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,12 +1,14 @@
 import ShopBanner from '@/components/ui/shop-banner';
 import ShopFooter from '@/components/ui/shop-footer';
 import ShopHeader from '@/components/ui/shop-header';
+import ShopTestimonials from '@/components/ui/shop-testimonials';
 
 export default function Shop() {
   return (
     <>
       <ShopHeader />
       <ShopBanner />
+      <ShopTestimonials />
       <ShopFooter />
     </>
   );
