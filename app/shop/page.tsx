3fedd2c66947942be4a81fccd16f2402ed2cbb67
Shinyn <mattias.alm@hotmail.com>
2026-03-26 19:32:22 +0100

    feat: Import ShopCategories

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,12 +3,14 @@ import ShopNewsLetter from '@/components/ui/shop-newsletter';
 import ShopFooter from '@/components/ui/shop-footer';
 import ShopHeader from '@/components/ui/shop-header';
 import ShopTestimonials from '@/components/ui/shop-testimonials';
+import ShopCategories from '@/components/ui/shop-categories';
 
 export default function Shop() {
   return (
     <>
       <ShopHeader />
       <ShopBanner />
+      <ShopCategories />
       <ShopTestimonials />
       <ShopNewsLetter />
       <ShopFooter />
