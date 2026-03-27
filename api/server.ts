97dbd1836177d32c804015a1fa709589d6bcf9e3
henrietta jeansson <henriettajeansson@gmail.com>
2026-03-27 12:34:21 +0100

    fix: add get product by id from db

--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,17 @@
 import cors from "cors";
 import express from "express";
-import usersRouter from "./routes/users.js"
+import usersRouter from "./routes/users.js";
+import productsRouter from "./routes/products.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 app.use(cors());
 app.use(express.json());
 
 
 app.use("/users", usersRouter);
+app.use("/products", productsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
