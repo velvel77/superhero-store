28e7385f1a9c843dd883bde147789de2e74702a3
Shinyn <mattias.alm@hotmail.com>
2026-03-30 14:56:30 +0200

    feat: Add base API fetch

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,3 +1,4 @@
+'use server';
 import ShopHeader from '@/components/ui/shop-header';
 import ShopBanner from '@/components/ui/shop-banner';
 import ShopCategories from '@/components/ui/shop-categories';
@@ -6,7 +7,18 @@ import ShopTestimonials from '@/components/ui/shop-testimonials';
 import ShopNewsLetter from '@/components/ui/shop-newsletter';
 import ShopFooter from '@/components/ui/shop-footer';
 
-export default function Shop() {
+export default async function Shop() {
+  const API_KEY = process.env.API_KEY;
+  try {
+    const response = await fetch(`https://superheroapi.com/api/${API_KEY}/1`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
   return (
     <>
       <ShopHeader />
