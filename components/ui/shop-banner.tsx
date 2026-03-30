d81a1c605b925538725785e2b1fbed36551f729d
Shinyn <mattias.alm@hotmail.com>
2026-03-30 14:51:34 +0200

    fix: Update color names

--- components/ui/shop-banner.tsx
+++ components/ui/shop-banner.tsx
@@ -9,7 +9,7 @@ const stats = [
 
 export default function ShopBanner() {
   return (
-    <section className="relative flex items-center border-b-2 border-white h-140">
+    <section className="relative flex items-center border-b-2 border-base-white h-140">
       <Image
         className="absolute -z-10 w-full object-cover bg-cover  h-140"
         src={'/hero-landing.jpg'}
@@ -21,19 +21,19 @@ export default function ShopBanner() {
       <div className="absolute inset-0 benday-dots z-10" aria-hidden="true"></div>
 
       {/* Gradient overlay on image */}
-      <div className="absolute inset-0 bg-linear-to-r from-(--color-effect-blue) via-(--color-effect-blue)/85 to-(--color-effect-blue)/40"></div>
+      <div className="absolute inset-0 bg-linear-to-r from-effect-blue via-effect-blue/85 to-effect-blue/40"></div>
 
       {/* Info overlay on image */}
       <div className="flex flex-col p-4 gap-4 w-full max-w-260 mx-auto z-10">
-        <span className="px-4 tracking-[.2rem] text-[.6rem] uppercase text-(--color-effect-red)">
+        <span className="px-4 tracking-[.2rem] text-[.6rem] uppercase text-effect-red">
           /// Kinetic voltage collection - 2026 ///
         </span>
         <h1 className="uppercase text-white text-6xl font-bold italic flex flex-col px-2 gap-3">
-          <span className="text-shadow-md text-shadow-(color:--color-effect-red)">Unleash your</span>{' '}
-          <span className="text-(--color-effect-yellow)">inner legend</span>
+          <span className="text-shadow-md text-shadow-effect-red">Unleash your</span>{' '}
+          <span className="text-effect-yellow">inner legend</span>
         </h1>
         <div className="relative flex items-center pl-6">
-          <span className="absolute left-4 h-0.5 w-20 bg-(--color-effect-red)"></span>
+          <span className="absolute left-4 h-0.5 w-20 bg-effect-red"></span>
           <small className="text-gray-500 px-4 py-4 pl-20">
             Premium heroic equipment & tactical gear for the modern vigilante
           </small>
@@ -41,11 +41,11 @@ export default function ShopBanner() {
 
         {/* Buttons */}
         <div className="text-white italic font-bold flex gap-2 px-4">
-          <button className="group hover:cursor-pointer uppercase bg-(--color-effect-red) flex gap-2 p-4 border-2 border-white">
+          <button className="group hover:cursor-pointer uppercase bg-effect-red flex gap-2 p-4 border-2 border-base-white">
             <Zap className="" /> Shop Now{' '}
             <ArrowRight className="transition-transform duration-150 group-hover:translate-x-1" />
           </button>
-          <button className="p-4 uppercase bg-(--color-effect-gray) border-2 border-white catalog-hover">
+          <button className="p-4 uppercase bg-effect-gray border-2 border-base-white catalog-hover">
             View Catalog
           </button>
         </div>
@@ -54,8 +54,8 @@ export default function ShopBanner() {
         <div className="flex gap-4 p-4">
           {stats.map((stat) => (
             <div key={stat.title}>
-              <span className="text-(--color-effect-yellow) font-bold pr-2 italic">{stat.number}</span>
-              <small className="text-gray-500 text-[.6rem] uppercase">{stat.title}</small>
+              <span className="text-effect-yellow font-bold pr-2 italic">{stat.number}</span>
+              <small className="text-basic-400 text-[.6rem] uppercase">{stat.title}</small>
             </div>
           ))}
         </div>
--- components/ui/shop-categories.tsx
+++ components/ui/shop-categories.tsx
@@ -39,12 +39,12 @@ const categories = [
 
 export default function ShopCategories() {
   return (
-    <section className="benday-dots bg-(--color-effect-blue) py-8">
+    <section className="benday-dots bg-effect-blue py-8">
       <div className=" mx-auto w-full max-w-260">
-        <div className="text-white relative p-4 grid items-center">
-          <div className="absolute mx-4 h-7 w-1 bg-(--color-effect-yellow)"></div>
+        <div className="text-base-white relative p-4 grid items-center">
+          <div className="absolute mx-4 h-7 w-1 bg-effect-yellow"></div>
           <h2 className="px-4">Gear Categories</h2>
-          <small className="px-4 text-(--color-text-muted)">Browse by division</small>
+          <small className="px-4 text-basic-400">Browse by division</small>
         </div>
 
         <div className="gap-6 place-items-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 p-4">
@@ -62,13 +62,13 @@ export default function ShopCategories() {
                 height={500}
                 loading="lazy"
               />
-              <div className="absolute inset-0 bg-linear-to-t from-(--color-effect-blue) via-(--color-effect-blue)/40 to-transparent"></div>
+              <div className="absolute inset-0 bg-linear-to-t from-effect-blue via-effect-blue/40 to-transparent"></div>
               <span className="absolute bottom-4 left-4">
                 <small className={`uppercase  ${item.textColor}`}>{item.count} items</small>
-                <h3 className="text-white font-bold italic">{item.name}</h3>
-                <div className="uppercase flex gap-2 items-center text-(--color-text-muted) text-[.6rem]">
-                  <span className="transition-colors duration-300 group-hover:text-white">Explore</span>{' '}
-                  <ArrowRight className="size-3 transition-all duration-300 group-hover:translate-x-1 group-hover:text-white" />
+                <h3 className="text-base-white font-bold italic">{item.name}</h3>
+                <div className="uppercase flex gap-2 items-center text-basic-400 text-[.6rem]">
+                  <span className="transition-colors duration-300 group-hover:text-base-white">Explore</span>{' '}
+                  <ArrowRight className="size-3 transition-all duration-300 group-hover:translate-x-1 group-hover:text-base-white" />
                 </div>
               </span>
             </Link>
