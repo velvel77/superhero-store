614361630580291f6300b1dd39e3dd82fff25f69
henrietta jeansson <henriettajeansson@gmail.com>
2026-04-01 17:37:47 +0200

    refactor: move newsletter form to component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,13 @@
 import ContactForm from "@/components/ui/contact-form";
+import NewsletterForm from "@/components/ui/newsletter-form";
 
 export default function ContactPage() {
 
     return (
-        < ContactForm />
+        <>
+            < ContactForm />
+
+            <NewsletterForm />
+        </>
     )
 }
--- components/ui/newsletter-form.tsx
+++ components/ui/newsletter-form.tsx
@@ -1,8 +1,8 @@
+import { subscribeNewsletter } from "@/app/contact/action";
 import Form from "next/form";
-import { subscribeNewsletter } from "../action";
 
-export default function NewsletterPage() {
-    return (
+export default function NewsletterForm(){
+    return(
         <Form action={subscribeNewsletter}>
             <label htmlFor="email">Subscribe to newsletter</label>
             <input type="email" name="email" placeholder="Your e mail..." required />
