8a160c3732655bcbd53b7018e975e682a3cda88f
Velvet <velvet.pinkish@gmail.com>
2026-04-07 14:40:59 +0200

    Fix: Update imports

--- api/dist/routes/superheroes.js
+++ api/dist/routes/superheroes.js
@@ -1,35 +1,28 @@
 import express from "express";
-import { pool } from "../db.js";
-
+import { pool } from "../db";
 const router = express.Router();
-
 router.get("/", async (req, res) => {
     try {
-        const { rows } = await pool.query(
-            `SELECT * FROM superheroes ORDER BY id DESC`
-        );
+        const { rows } = await pool.query(`SELECT * FROM superheroes ORDER BY id DESC`);
         res.json(rows);
-    } catch (error) {
+    }
+    catch (error) {
         console.error("DB error:", error);
         res.status(500).json({ error: "Failed to fetch superheroes" });
     }
 });
-
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
     try {
-        const { rows } = await pool.query(
-            `SELECT * FROM superheroes WHERE id = $1`,
-            [id]
-        );
+        const { rows } = await pool.query(`SELECT * FROM superheroes WHERE id = $1`, [id]);
         if (rows.length === 0) {
             return res.status(404).json({ error: "Superhero not found" });
         }
         res.json(rows[0]);
-    } catch (error) {
+    }
+    catch (error) {
         console.error("DB error: ", error);
         res.status(500).json({ error: "Failed to fetch superhero" });
     }
-})
-
+});
 export default router;
---
+++ api/dist/server.js
@@ -0,0 +1,23 @@
+import path from "node:path";
+import dotenv from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
+}
+import cors from "cors";
+import express from "express";
+import categoryRouter from "./routes/categories.js";
+import productsRouter from "./routes/products.js";
+import superheroRouter from "./routes/superheroes.js";
+import usersRouter from "./routes/users.js";
+const app = express();
+const PORT = process.env.PORT || 5000;
+console.log("ENV PORT:", process.env.PORT);
+app.use(cors());
+app.use(express.json());
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/categories", categoryRouter);
+app.use("/superheroes", superheroRouter);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
