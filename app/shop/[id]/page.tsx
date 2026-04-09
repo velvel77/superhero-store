9fc9bcca3b685a3abcb8cc1f75ed392474082cb2
Shinyn <mattias.alm@hotmail.com>
2026-04-09 11:01:56 +0200

    Feat: Replace API call with scraped data in json file

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -1,14 +1,20 @@
 import Superhero from '@/components/ui/shop-superhero';
+import heroes from '@/data/heroes.json';
 
 export default async function Product({ params }: { params: Promise<{ id: string }> }) {
-  const API_KEY = process.env.API_KEY;
+  // const API_KEY = process.env.API_KEY;
   const { id } = await params;
-  console.log('This is the id from dynamic page', id);
-  const res = await fetch(`https://superheroapi.com/api/${API_KEY}/${id}`);
-  const hero = await res.json();
+  // console.log('This is the id from dynamic page', id);
+  // const res = await fetch(`https://superheroapi.com/api/${API_KEY}/${id}`);
+  // const hero = await res.json();
 
-  if (hero.response === 'error') {
-    throw new Error('Hero not found');
+  // if (hero.response === 'error') {
+  //   throw new Error('Hero not found');
+  // }
+
+  const hero = heroes.find((h) => h.id === id);
+  if (!hero) {
+    return <div>Hero not found</div>;
   }
 
   return <Superhero hero={hero} />;
