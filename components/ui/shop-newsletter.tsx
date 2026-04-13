e871c84b4833f6f0c1cb31cbdb324b5559253a4a
Shinyn <mattias.alm@hotmail.com>
2026-04-13 18:03:33 +0200

    Fix: Update colors

--- components/ui/shop-newsletter.tsx
+++ components/ui/shop-newsletter.tsx
@@ -5,7 +5,7 @@ export default function ShopNewsLetter() {
     <section className="diagonal-stripes flex justify-between border-b-2 border-ui-border bg-secondary-500 px-4 py-16">
       <div className="flex justify-between w-full max-w-260 mx-auto">
         <div className="flex items-center gap-2">
-          <Zap className="text-basic-100 border border-ui-border p-2 size-10 rounded-sm" />
+          <Zap className="text-basic-100 border border-basic-100 p-2 size-10 rounded-sm" />
           <div>
             <h2 className="uppercase font-bold text-basic-100 italic">Join the league</h2>
             <p className="text-basic-300 uppercase text-[.6rem]">
@@ -14,7 +14,12 @@ export default function ShopNewsLetter() {
           </div>
         </div>
         <form className="flex border-2 border-ui-border/80" action="">
-          <input className="outline-0 p-2 text-sm text-basic-100 placeholder:text-basic-200" required type="email" placeholder="hero@email.com" />
+          <input
+            className="outline-0 p-2 text-sm text-basic-100 placeholder:text-basic-200"
+            required
+            type="email"
+            placeholder="hero@email.com"
+          />
           <button
             className="p-2 flex items-center gap-2 hover:bg-basic-200 bg-basic-100 text-secondary-500"
             type="submit"
