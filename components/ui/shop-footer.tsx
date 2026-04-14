ec7000e123c5ca9c9d880f732d2f4c811dfb64ad
Shinyn <mattias.alm@hotmail.com>
2026-04-14 21:00:41 +0200

    Feat: Add disclaimer

--- components/ui/shop-footer.tsx
+++ components/ui/shop-footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function ShopFooter() {
   return (
     <footer className="p-4 bg-basic-700 benday-dots">
-      <div className="flex pb-8 mx-auto max-w-260">
+      <section className="flex pb-8 mx-auto max-w-260">
         <div>
           <div className="flex gap-1 py-2 text-basic-100">
             <Zap className="bg-secondary-500 border rounded-xs p-1 size-6 border-basic-100" />
@@ -64,11 +64,22 @@ export default function ShopFooter() {
             </li>
           </ul>
         </nav>
-      </div>
+      </section>
       <div className="py-4 text-basic-400 uppercase text-[.6rem] flex justify-between max-w-260 mx-auto border-t border-ui-border">
         <span>© 2026 Hero Vault — Gear Division</span>
         <span>Plate 001 / Kinetic Voltage Series</span>
       </div>
+      <section className="w-full mt-4 max-w-260 mx-auto">
+        <span className="text-basic-100">Disclaimer</span>
+        <p className="text-xs text-basic-400">
+          This website is a non-commercial project created for educational and portfolio purposes. All characters,
+          images, and written content are generated using artificial intelligence and are intended to be original works.
+          Any resemblance to existing characters, trademarks, or intellectual property is coincidental and
+          unintentional. This project is not affiliated with, endorsed by, or connected to any companies, brands, or
+          rights holders. If you believe any content may infringe upon your rights, please contact us and it will be
+          reviewed and removed if necessary.
+        </p>
+      </section>
     </footer>
   );
 }
