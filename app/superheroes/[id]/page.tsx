26f1c59954484726b8341cda142c417acb001ded
Velvet <velvet.pinkish@gmail.com>
2026-04-14 18:22:55 +0200

    Fix: Use variable instead of hardcoded value for dynamic routing

--- app/superheroes/[id]/page.tsx
+++ app/superheroes/[id]/page.tsx
@@ -1,9 +1,27 @@
-import Superhero from '@/components/ui/shop-superhero';
-import type { Hero } from '@/app/types';
+import type { Hero } from "@/app/types";
+import Superhero from "@/components/ui/shop-superhero";
 
-export default async function Product({ params }: { params: Promise<{ id: string }> }) {
+export default async function Product({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
-  const data = await fetch('http://localhost:5000/superheroes');
+  const apiUrl = process.env.API_URL;
+
+  if (!apiUrl) {
+    throw new Error("API_URL is not set");
+  }
+
+  const data = await fetch(`${apiUrl}/superheroes`);
+
+  if(!data.ok) {
+    const body = await data.text();
+    throw new Error(
+      `Failed to fetch superheroes: ${data.status} ${data.statusText}. Body ${body}`
+    )
+  }
+
   const heroes = await data.json();
 
   const hero = heroes.find((h: Hero) => h.id === Number(id));
