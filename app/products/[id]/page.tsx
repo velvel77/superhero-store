4d3acbd6ea9e0612bf3ee470c26af4004c3d0fc4
henrietta jeansson <henriettajeansson@gmail.com>
2026-04-16 18:47:39 +0200

    feat: add metadata to products:id

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import {
+	getProductById,
 	getProductDetailsById,
 	getRelatedProducts,
 } from "@/lib/queries/products";
@@ -10,6 +11,15 @@ type Props = {
 		id: string;
 	}>;
 };
+
+export async function generateMetadata({ params }: PageProps<"/products/[id]">) {
+	const { id } = await params;
+	const product = await getProductById(Number(id));
+	return {
+		title: `Superhero Store - ${product.name}`,
+		description: product.description
+	}
+}
 
 export default async function ProductPage({ params }: Props) {
 	const { id } = await params;
