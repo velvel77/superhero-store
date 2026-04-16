3f4996dd2250d288f0a28679d76aaf2f0e7e3a29
Velvet <velvet.pinkish@gmail.com>
2026-04-16 11:06:29 +0200

    Add type to cart-buttons

--- components/ui/cart-button.tsx
+++ components/ui/cart-button.tsx
@@ -7,8 +7,8 @@ export default function CartButton({ item }: { item: CartItem }) {
     const { items, addItem, removeItem } = useCart();
     const inCart = items.some(i => i.id === item.id && i.type === "product");
     return inCart ? (
-        <button onClick={() => removeItem(item.id, item.type)}>Remove</button>
+        <button type="button" onClick={() => removeItem(item.id, item.type)}>Remove</button>
     ) : (
-        <button onClick={() => addItem(item)}>Add to cart</button>
+        <button type="button" onClick={() => addItem(item)}>Add to cart</button>
     )
 }
