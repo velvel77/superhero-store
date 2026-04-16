4c281927eb69b54a7baabf2db69b19fb0fcef295
Velvet <velvet.pinkish@gmail.com>
2026-04-16 15:01:38 +0200

    Feature: Import header and footer components for cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,21 +1,26 @@
 import CartList from "@/components/ui/cart-list";
-import { getCart } from "./actions";
 import PlaceOrderButton from "@/components/ui/place-order-button";
+import ShopFooter from "@/components/ui/shop-footer";
+import ShopHeader from "@/components/ui/shop-header";
+import ShopNewsLetter from "@/components/ui/shop-newsletter";
+import { getCart } from "./actions";
 
 export default async function CartPage() {
-    const cart = await getCart();
+  const cart = await getCart();
 
-    if (cart.length === 0) {
-        return <p>Your cart is empty.</p>
-
-
-    }
-    return (
-
-        <div>
-            <h1>Your cart</h1>
-            <CartList />
-            <PlaceOrderButton />
-        </div>
-    )
+  if (cart.length === 0) {
+    return <p>Your cart is empty.</p>;
+  }
+  return (
+    <>
+      <ShopHeader />
+      <div>
+        <h1>Your cart</h1>
+        <CartList />
+        <PlaceOrderButton />
+      </div>
+      <ShopNewsLetter />
+      <ShopFooter />
+    </>
+  );
 }
